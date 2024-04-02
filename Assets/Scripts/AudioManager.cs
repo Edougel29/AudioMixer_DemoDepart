62716ca9c8b414717581f86488182fcd3e053fda
Edougel29 <edougel29@gmail.com>
2024-04-02 14:11:16 -0400

    sa bug

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine; Audio;
+using UnityEngine; UI;
+
+public class AudioManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioMixer audioMixer;
+
+    [SerializeField]
+    private Slider glissiereVolumeMusique
+
+
+    void Start()
+    {
+        
+        //audioMixer.GetFloat("MusiqueVolume");
+        //audioMixer.SetFloat("MusiqueVolume", -20f);
+
+    }
+
+    public void AjusteVolumeMusique(float volume){
+
+
+
+
+    }
+
+
+
+}
